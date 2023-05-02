2db140b6ba19f56d6f835211daa4f9dab3d888cc
PrachetShah <prachetshah25@gmail.com>
2023-05-03 00:25:15 +0530

    added functionality

--- etherscan.py
+++ etherscan.py
@@ -13,33 +13,34 @@ driver = webdriver.Chrome()
 rows = []
 fieldnames= ["1","Txn Hash", "Method", "Block","2", "Age","3", "From","4", "To", "Value", "Txn Fee","5"]
 
-pages = 2
 
-for i in range(1, pages):
-    driver.get(f'https://etherscan.io/txs?p={i}')
-    time.sleep(5)
-    try:
-        table = driver.find_element(By.XPATH, """//*[@id="ContentPlaceHolder1_divTransactions"]/div[2]/table""")
+# pages = 2
+def scrape(pages=2):
+    for i in range(1, pages):
+        driver.get(f'https://etherscan.io/txs?p={i}')
+        time.sleep(5)
+        try:
+            table = driver.find_element(By.XPATH, """//*[@id="ContentPlaceHolder1_divTransactions"]/div[2]/table""")
 
-        for tr in table.find_element(By.XPATH, './tbody').find_elements(By.TAG_NAME, 'tr'):
-            row = [td.text.strip() for td in tr.find_elements(By.TAG_NAME, 'td')]
-            rows.append(row)
-        print(f"{i} Iteration Completed")
-    except Exception as e:
-        print(e)
-    if(i%20==0):
-        df = pd.DataFrame(rows, columns = fieldnames)
-        print(df.head())
-        timestamp = datetime.datetime.now().strftime ("%Y%m%d_%H%M%S")
-        df.to_csv(f"transactions-{timestamp}-{i}.csv")
-        rows = []
+            for tr in table.find_element(By.XPATH, './tbody').find_elements(By.TAG_NAME, 'tr'):
+                row = [td.text.strip() for td in tr.find_elements(By.TAG_NAME, 'td')]
+                rows.append(row)
+            print(f"{i} Iteration Completed")
+        except Exception as e:
+            print(e)
+        if(i%20==0):
+            df = pd.DataFrame(rows, columns = fieldnames)
+            print(df.head())
+            timestamp = datetime.datetime.now().strftime ("%Y%m%d_%H%M%S")
+            df.to_csv(f"transactions-{timestamp}-{i}.csv")
+            rows = []
+    df = pd.DataFrame(rows, columns = fieldnames)
+    print(df.head())
+    # timestamp = datetime.datetime.now().strftime ("%Y%m%d_%H%M%S")
 
-df = pd.DataFrame(rows, columns = fieldnames)
-print(df.head())
-timestamp = datetime.datetime.now().strftime ("%Y%m%d_%H%M%S")
+    # data = 'data'
+    # path = os.path.join(data, f"transactions-{timestamp}-{i}.csv")
 
-data = 'data'
-path = os.path.join(data, f"transactions-{timestamp}-{i}.csv")
-
-df.to_csv(path)
+    # df.to_csv(path)
+    return df
 
--- main.py
+++ main.py
@@ -6,12 +6,14 @@ author SparkByExamples.com
 import pandas as pd    
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType,StructField, StringType, IntegerType
+from etherscan import *
 
 def show():
     data = [['Scott', 50], ['Jeff', 45], ['Thomas', 54],['Ann',34]] 
 
-    pandasDF = pd.DataFrame(data, columns = ['Name', 'Age']) 
+    # pandasDF = pd.DataFrame(data, columns = ['Name', 'Age']) 
 
+    pandasDF = scrape(3)
     spark = SparkSession.builder \
         .master("local[1]") \
         .appName("SparkByExamples.com") \
